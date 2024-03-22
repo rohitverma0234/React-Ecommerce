e2ee918350614e74cb196889b949ccfe401b564c
Rohit Verma <rohitverma0150@gmail.com>
2024-03-22 17:54:39 -0400

    Components:- Header.js, Nav.js, install react-icons.

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,4 +1,81 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Work Sans", sans-serif;
+}
+
+
+html {
+  font-size: 62.5%;
+  /* scroll-behavior: smooth; */
+  /* 1rem = 10px */
+  overflow-x: hidden;
+}
+
+body {
+  overflow-x: hidden;
+   scrollbar-color: rgb(98 84 243);
+    scrollbar-width: thin;
+}
+
+body::-webkit-scrollbar {
+  width: 1.5rem;
+}
+
+body::-webkit-scrollbar-track {
+   background-color: rgb(24 24 29);
+}
+
+body::-webkit-scrollbar-thumb {
+ 
+  background: #fff;
+    border: 5px solid transparent;
+    border-radius: 9px;
+    background-clip: content-box;
+}
+
+h1,
+h2,
+h3,
+h4 {
+   font-family: "Work Sans", sans-serif;
+
+}
+
+h1 {
+  color: ${({ theme }) => theme.colors.heading};
+  font-size: 6rem;
+  font-weight: 900;
+}
+
+ h2 {
+   color: ${({ theme }) => theme.colors.heading};
+   font-size: 4.4rem;
+   font-weight: 300;
+   white-space: normal;
+  
+  }
+
+h3 {
+  font-size: 1.8rem;
+  font-weight: 400;
+}
+
+p, button {
+  color: ${({ theme }) => theme.colors.text};
+  font-size: 1.65rem;
+  line-height: 1.5;
+  font-weight:400;
+}
+
+a {
+  text-decoration: none;
+}
+
+li {
+  list-style: none;
+}`
