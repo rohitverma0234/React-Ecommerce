511aa8ca57c5ce001e8cc67a19a338dd18047245
rohitverma0234 <rohitverma0150@gmail.com>
2024-03-31 08:26:05 -0400

    Created - Feature Section using map function

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from './components/HeroSection'
 import Services from './components/Services'
 import Trusted from './components/Trusted'
+import FeatureProduct from './components/FeatureProduct'
 
 const Home = () => {
   const data = {
@@ -11,6 +12,7 @@ const Home = () => {
   return (
     <>
     <HeroSection myData={data}/>
+    <FeatureProduct/>
     <Services/>
     <Trusted/>
     </>
