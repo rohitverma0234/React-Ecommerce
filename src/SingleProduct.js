26a4f2652eb5eb3e70489ea68b1cdf62ae0b67f1
rohitverma0234 <rohitverma0150@gmail.com>
2024-04-06 20:52:26 -0400

    Created - Star Ratings and Reviews

--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -8,6 +8,7 @@ import { Container } from './styles/Container'
 import FormatPrice from './Helpers/FormatPrice'
 import { MdSecurity } from "react-icons/md";
 import { TbTruckDelivery, TbReplace } from "react-icons/tb";
+import Star from './components/Star'
 
 const API = "https://api.pujakaitem.com/api/products"
 
@@ -43,8 +44,7 @@ const SingleProduct = () => {
           {/* Product Data */}
           <div className='product-data'>
             <h2>{name}</h2>
-            <p>{stars}</p>
-            <p>{reviews}</p>
+            <Star stars={stars} reviews={reviews}/>
             <p className='product-data-price'>
               USD:
               <del>
