d10a13f81f839831ec51f8ba6f050e1d3e4958ad
rohitverma0234 <rohitverma0150@gmail.com>
2024-04-15 11:54:42 -0400

    Grid View Layout in Prodct Page

--- src/Products.js
+++ src/Products.js
@@ -6,8 +6,6 @@ import ProductList from './components/ProductList';
 import { useFilterContext } from './context/filter_context';
 
 const Products = () => {
-  const {filter_products} = useFilterContext();
-  console.log(filter_products)
   return (
     <Wrapper>
       <div className="container grid grid-filter-column">
