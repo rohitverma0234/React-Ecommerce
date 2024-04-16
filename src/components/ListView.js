9eed2746fde24f60f70fb692c8d0147da296be22
rohitverma0234 <rohitverma0150@gmail.com>
2024-04-16 09:39:28 -0400

    Created - ListView Component

---
+++ src/components/ListView.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from "styled-components"
+
+const ListView = () => {
+  return (
+    <Wrapper className="section">
+    <div className='container grid'></div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+    `
+
+export default ListView
