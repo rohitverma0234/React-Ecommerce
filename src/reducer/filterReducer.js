8133bc4593f1806b958ced2ec3b4aaaa0b61767b
rohitverma0234 <rohitverma0150@gmail.com>
2024-04-24 11:39:00 -0400

    make code shorter

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -28,39 +28,67 @@ const filterReducer = (state, action) => {
                 sorting_value: sort_value,
             }
 
+        // case "SORTING_PRODUCTS":
+        //     let newSortData;
+        //     let tempSortProduct = [...action.payload]
+
+        //     if (state.sorting_value === "lowest"){
+        //         const sortingProducts = (a,b) =>{
+        //             return a.price - b.price
+        //         }
+        //         newSortData = tempSortProduct.sort(sortingProducts)
+        //     }
+
+        //     if (state.sorting_value === "highest"){
+        //         const sortingProducts = (a,b) =>{
+        //             return b.price - a.price
+        //         }
+        //         newSortData = tempSortProduct.sort(sortingProducts)
+        //     }
+
+        //     if (state.sorting_value === "a-z"){
+        //         newSortData = tempSortProduct.sort((a,b)=>{
+        //             return a.name.localeCompare(b.name)
+        //         })
+        //     }
+
+        //     if (state.sorting_value === "z-a"){
+        //         newSortData = tempSortProduct.sort((a,b)=>{
+        //             return b.name.localeCompare(a.name)
+        //         })
+        //     }
+
+
         case "SORTING_PRODUCTS":
-            let newSortData;
-            let tempSortProduct = [...action.payload]
+            const tempSortProduct = [...action.payload];
 
-            if (state.sorting_value === "lowest"){
-                const sortingProducts = (a,b) =>{
-                    return a.price - b.price
+            // Helper function to determine the sorting method
+            const getSortFunction = (sortingValue) => {
+                switch (sortingValue) {
+                    case "lowest":
+                        return (a, b) => a.price - b.price;
+                    case "highest":
+                        return (a, b) => b.price - a.price;
+                    case "a-z":
+                        return (a, b) => a.name.localeCompare(b.name);
+                    case "z-a":
+                        return (a, b) => b.name.localeCompare(a.name);
+                    default:
+                        return undefined;  // or some default sorting method
                 }
-                newSortData = tempSortProduct.sort(sortingProducts)
-            }
+            };
 
-            if (state.sorting_value === "highest"){
-                const sortingProducts = (a,b) =>{
-                    return b.price - a.price
-                }
-                newSortData = tempSortProduct.sort(sortingProducts)
-            }
+            // Determine the correct sorting function based on sorting_value
+            const sortingFunction = getSortFunction(state.sorting_value);
 
-            if (state.sorting_value === "a-z"){
-                newSortData = tempSortProduct.sort((a,b)=>{
-                    return a.name.localeCompare(b.name)
-                })
-            }
+            // Apply the sorting function
+            let newSortData = tempSortProduct.sort(sortingFunction);
+            
 
-            if (state.sorting_value === "z-a"){
-                newSortData = tempSortProduct.sort((a,b)=>{
-                    return b.name.localeCompare(a.name)
-                })
+            return {
+                ...state,
+                filter_products: newSortData
             }
-        return {
-            ...state,
-            filter_products:newSortData
-        }
 
         default:
             return state;
