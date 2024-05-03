c880bbe6cb8fca6ef6d9b874234f3ef69643960b
rohitverma0234 <rohitverma0150@gmail.com>
2024-05-03 16:30:25 -0400

    Added - filter-category

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -77,11 +77,17 @@ const filterReducer = (state, action) => {
             let {all_products} = state
             let tempFilterProduct = [...all_products]
 
-            const {text} = state.filters;
+            const {text, category} = state.filters;
             
             if(text){
                 tempFilterProduct = tempFilterProduct.filter((curElem)=>{
                     return curElem.name.toLowerCase().includes(text)
+                })
+            }
+
+            if(category){
+                tempFilterProduct = tempFilterProduct.filter((curElem)=>{
+                    return curElem.category=== category;
                 })
             }
 
