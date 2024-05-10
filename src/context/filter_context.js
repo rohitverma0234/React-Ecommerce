9182b8243438c119844c6cfba4ba3088e5339d05
rohitverma0234 <rohitverma0150@gmail.com>
2024-05-10 16:53:32 -0400

    Created -- color & price filter

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,7 +15,11 @@ export const FilterContextProvider = ({ children }) => {
         filters: {
             text: "",
             category:"all",
-            company:"all"
+            company:"all",
+            color:"all",
+            maxPrice:0,
+            price:0,
+            minPrice:0
         }
     }
 
